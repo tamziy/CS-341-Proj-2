8addc276631c76925d789931435b73a593e0a25f
tamziy <114886626+tamziy@users.noreply.github.com>
2023-10-18 22:30:15 -0500

    Starter Files

---
+++ Library.fs
@@ -0,0 +1,55 @@
+// Work on your solution here
+
+// Do not change the API (module/namespace/function signatures)
+
+// Refer to the PDF for more instructions on how to implement
+// each function
+
+// As of now, each function simply takes an image as input
+// and returns it as it is
+
+namespace ImageLibrary
+
+module Operations =
+ 
+  let rec Grayscale (width:int) 
+                    (height:int) 
+                    (depth:int) 
+                    (image:(int*int*int) list list) = 
+    
+    image
+
+
+  
+  let rec Threshold (width:int) 
+                    (height:int)
+                    (depth:int)
+                    (image:(int*int*int) list list)
+                    (threshold:int) = 
+    
+    image
+
+
+  
+  let rec FlipHorizontal (width:int)
+                         (height:int)
+                         (depth:int)
+                         (image:(int*int*int) list list) = 
+    image
+
+
+  
+  let rec EdgeDetect (width:int)
+               (height:int)
+               (depth:int)
+               (image:(int*int*int) list list)
+               (threshold:int) = 
+    image
+
+
+  let rec RotateRight90 (width:int)
+                        (height:int)
+                        (depth:int)
+                        (image:(int*int*int) list list) = 
+    image
+
